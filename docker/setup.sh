64030c52200d49fe60ddd31c3d38ca273a0f1587
Clément Rigo <rigoclement@mydnic.be>
2025-06-09 21:15:26 +0200

    Update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -6,5 +6,11 @@ if [ ! -f .env ]; then
     cp .env.docker .env
 fi
 
+# Demander le chemin vers les fichiers musicaux
+read -p "Entrez le chemin absolu vers votre dossier de musique: " music_path
+
+# Mettre à jour le fichier .env avec le chemin de musique
+sed -i "s|/path/to/music|$music_path|g" docker-compose.yml
+
 echo "Configuration terminée. Vous pouvez maintenant lancer l'application avec:"
 echo "docker-compose up -d"
